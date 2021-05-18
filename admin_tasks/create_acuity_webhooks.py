3e8eb649a4420fcac9d3c6d782b63e1ea8b0ca7e
André Sartori <afs25@cam.ac.uk>
2021-05-18 13:09:22 +0100

    #3979 comment: Edited create_acuity_webhooks.py

--- admin_tasks/create_acuity_webhooks.py
+++ admin_tasks/create_acuity_webhooks.py
@@ -1,11 +1,30 @@
+#
+#   Thiscovery API - THIS Institute’s citizen science platform
+#   Copyright (C) 2019 THIS Institute
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   A copy of the GNU Affero General Public License is available in the
+#   docs folder of this project.  It is also available www.gnu.org/licenses/
+#
+import local.dev_config
+import local.secrets
 import thiscovery_lib.utilities as utils
 from src.common.acuity_utilities import AcuityClient
 
 
 EVENTS = [
-    'appointment.scheduled',
-    'appointment.rescheduled',
-    'appointment.canceled',
+    "appointment.scheduled",
+    "appointment.rescheduled",
+    "appointment.canceled",
     # 'appointment.changed',
 ]
 
@@ -15,11 +34,13 @@ def main():
     ac = AcuityClient()
     for e in EVENTS:
         try:
-            response = ac.post_webhooks(e)
-            logger.info('Created webhook', extra={'response': response})
+            response = ac.post_webhooks(
+                e, target=f"{local.dev_config.AWS_TEST_API}v1/appointment-event"
+            )
+            logger.info("Created webhook", extra={"response": response})
         except utils.DetailedValueError:
             pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
